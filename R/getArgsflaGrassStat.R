5f199fb73ef76d44ebd9bc5b404252cc902b380c
Folke Larsson <fl_blogs@fldbd.com>
2018-03-19 14:08:22 +0100

    adjust documentation

--- R/getArgsflaGrassStat.R
+++ R/getArgsflaGrassStat.R
@@ -1,18 +1,17 @@
 # TODO: Add comment
 # 
-# Author: follar
+# Author: Folke Larsson
 ###############################################################################
 
 
-#' @title getArgsGstat package
+#' @title getArgsflaGrassStat package
 #' @description Returning a dataframe with arguments after reading from an ini-file or from command prompt. Part of project calculating statistics from shape-points and interpreted satelite-images using Grass and GDAL, Today called flaGrassStat. 
-#' @author Folke Larsson \, Boden Sweden
-#' @section getArgdGstat functions:
+#' @author Folke Larsson in Boden Sweden
+#' @section getArgsflaGrassStat functions:
 #' read_ini_file
 #' read_arguments
 #' 
 #' @docType package
-#' @name getArgsGstat
-#' 
+#' @name getArgsflaGrassStat
 #' 
 NULL
